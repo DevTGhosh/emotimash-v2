60ea217e2d5c85cdba4980bba3158e276e92d278
Dev <devjyotighosh02@gmail.com>
2020-05-04 13:25:19 +0530

    Updated styling of 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,17 +3,18 @@ import React from "react";
 import abductionIllustration from "../images/abduction-illustration.svg";
 
 function NotFoundPage() {
+  console.log("abductionIllustration: ", typeof abductionIllustration);
   return (
-      <div>
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={abductionIllustration}
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
-        </h2>
-      </div>
+    <div className="w-screen h-screen grid items-center justify-center">
+      <img
+        alt="Ghost getting abducted by aliens"
+        className="block mx-auto w-1/2"
+        src={abductionIllustration}
+      />
+      <h2 className="bg-yellow-400 text-2xl font-bold inline-block self-start p-3 text-center">
+        Looks like this page is a ghost that got abducted by aliens...
+      </h2>
+    </div>
   );
 }
 

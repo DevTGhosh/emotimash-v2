866fb239916e5468f39e8b688070db3615914c73
Dev <devjyotighosh02@gmail.com>
2020-05-18 17:14:23 +0530

    Changed mediquery for EmojiSelection

--- src/components/EmojiSelection.js
+++ src/components/EmojiSelection.js
@@ -1,31 +1,41 @@
 import React from "react";
 import styled from "@emotion/styled";
 // import { css } from "@emotion/core";
-import { mq } from "../utilities/ui";
+import { mq, mqf } from "../utilities/ui";
 import { startingEmoji } from "../utilities/images";
 
-const EmojiContainer = styled.div({
-  background: `#38a169`,
-  margin: `3rem 0`,
-  width: `83.33%`,
-  height: `18.75rem`,
-  display: `grid`,
-  gridTemplateColumns: `repeat(3, 1fr)`,
-  alignContent: `space-around`,
-  justifyItems: `center`,
-  [mq[2]]: {
-    gridRowStart: 1,
-    gridRowEnd: 3,
+const EmojiContainer = styled.div(
+  {
+    margin: `3rem 0`,
+    width: `83.33%`,
+    height: `18.75rem`,
+    display: `grid`,
+    gridTemplateColumns: `repeat(3, 1fr)`,
+    alignContent: `space-around`,
+    justifyItems: `center`,
+    [mq[2]]: {
+      gridRowStart: 1,
+      gridRowEnd: 3,
+    },
   },
-});
+  mqf({
+    width: [`18.75rem`, `30rem`, `30rem`, `40rem`],
+    height: [`18.75rem`, `25rem`, `30rem`, `40rem`],
+  })
+);
 
-const EmojiImageDiv = styled.div({
-  width: `75px`,
-  height: `75px`,
-});
-const Img = styled.img({
-  background: `white`,
-});
+const EmojiImageDiv = styled.div(
+  {
+    width: `75px`,
+    height: `75px`,
+    background: `white`,
+  },
+  mqf({
+    width: [`75px`, `90px`, `110px`],
+    height: [`75px`, `90px`, `110px`],
+  })
+);
+const Img = styled.img({});
 
 const EmojiSelection = () => {
   return (
--- src/utilities/ui.js
+++ src/utilities/ui.js
@@ -1,3 +1,9 @@
+import facepaint from "facepaint";
+
 const breakpoints = [640, 768, 1024, 1280];
 
 export const mq = breakpoints.map((bp) => `@media (min-width: ${bp}px)`);
+
+export const mqf = facepaint(
+  breakpoints.map((bp) => `@media (min-width: ${bp}px)`)
+);

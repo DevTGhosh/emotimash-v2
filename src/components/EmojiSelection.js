4dcc28eb69c1ae5c2353720e4b148e2d959b5f44
Dev <devjyotighosh02@gmail.com>
2020-05-21 21:29:44 +0530

    Saves emoji selection to Context

--- src/components/EmojiSelection.js
+++ src/components/EmojiSelection.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "@emotion/styled";
 // import { css } from "@emotion/core";
+import { store } from "../store";
 import { mq, mqf } from "../utilities/ui";
 import { startingEmoji } from "../utilities/images";
 
@@ -36,14 +37,61 @@ const EmojiImageDiv = styled.div(
   })
 );
 const Img = styled.img({});
-
+const emojiDispatch = (dispatch, index, selectedEmojiState) => {
+  const len = selectedEmojiState.length;
+  let valueExists = false;
+  //checks if selected emoji already exists in the state
+  for (let i = 0; i < len; i++) {
+    const element = selectedEmojiState[i];
+    if (element === index) {
+      valueExists = true;
+    }
+  }
+  if (len < 3 && !valueExists) {
+    if (len < 1) {
+      dispatch({ type: "SELECT_EMOJI", emojiIndex: index });
+    } else if (len < 2) {
+      dispatch({ type: "SELECT_EMOJI", emojiIndex: index });
+      //dispatch final emoji
+    } else {
+      //Removing the 2nd emoji in the state
+      dispatch({ type: "REMOVE_EMOJI", selectEmoji: 1 });
+      dispatch({ type: "SELECT_EMOJI", emojiIndex: index });
+      //dispatch Final emoji
+    }
+  } else {
+    return null;
+  }
+};
+const setAltImg = (index) => {
+  const imgAltArr = [
+    "Blush emoji",
+    "Grin emoji",
+    "Happy emoji",
+    "Heart Eyes emoji",
+    "Ninja emoji",
+    "Puke emoji",
+    "Sad  emoji",
+    "Sleep emoji",
+    "Tongue Out emoji",
+  ];
+  return imgAltArr[index];
+};
 const EmojiSelection = () => {
+  const globalState = useContext(store);
+  const selectedEmojiState = globalState.state[0].emoji;
+  const { dispatch } = globalState;
   return (
     <EmojiContainer>
       {startingEmoji.map((emoji, index) => {
         return (
-          <EmojiImageDiv key={index}>
-            <Img src={emoji} />
+          <EmojiImageDiv
+            key={index}
+            onClick={() => {
+              emojiDispatch(dispatch, index, selectedEmojiState);
+            }}
+          >
+            <Img src={emoji} alt={setAltImg(index)} />
           </EmojiImageDiv>
         );
       })}
